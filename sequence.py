c9089163de6a463671fe1f58a996e8cecad8a912
unknown <lorrainert2002@gmail.com>
2021-09-02 16:02:25 +0000

    project sequence added in

--- sequence.py
+++ sequence.py
@@ -1,9 +1,13 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-nums=[]
-answer=0
+a = 0
+b = 1
+c = 2
 for i in range(1,n+1):
     if i < 3:
         print(i)
     else:
-        i+=(i-1) + (i-2) + (i-3)
-        print(i)
+        answer = a+b+c
+        print(answer)
+        a = b
+        b = c
+        c = answer
